f0905bf4b3aba86bf596e570350f4c9ae3f9501a
Suin Yun <suinyun@lawn-128-61-1-94.lawn.gatech.edu>
2018-02-27 23:19:02 -0500

    final M5

--- app/src/main/java/edu/team27/perfectcube/controller/RegisterActivity.java
+++ app/src/main/java/edu/team27/perfectcube/controller/RegisterActivity.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckedTextView;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -24,7 +26,7 @@ public class RegisterActivity extends AppCompatActivity {
     EditText username;
     EditText password;
     TextView warning;
-    CheckedTextView userTypeCheckBox;
+    Spinner userTypeSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +37,13 @@ public class RegisterActivity extends AppCompatActivity {
         username = findViewById(R.id.usernameText);
         password = findViewById(R.id.passwordText);
         warning = findViewById(R.id.warning);
-        userTypeCheckBox = findViewById(R.id.checkBox);
+        userTypeSpinner = findViewById(R.id.spinner);
+        /*
+          Set up the adapter to display the allowable majors in the spinner
+         */
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, UserType.values());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        userTypeSpinner.setAdapter(adapter);
 
         warning.setVisibility(View.GONE);
 
@@ -45,13 +53,7 @@ public class RegisterActivity extends AppCompatActivity {
 
                 ArrayList<String> users = LoginData.getUsers();
                 ArrayList<String> passwords = LoginData.getPasswords();
-                
 
-                UserType usertype;
-
-                if (userTypeCheckBox.isChecked()) {
-                    usertype = UserType.ADMINISTRATOR;
-                }
 
 
                 if(users.contains(username.getText().toString())) {

d947a3dbc4f8ae14a1011d7ab757e429a507cde6
emmadoksansky <emma.doksansky@gmail.com>
2018-03-08 16:56:21 -0500

    added clickability to shelter name. deleted unnecessary files.

---
+++ app/src/main/java/edu/team27/perfectcube/controller/ListActivity.java
@@ -0,0 +1,66 @@
+package edu.team27.perfectcube.controller;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.team27.perfectcube.R;
+import edu.team27.perfectcube.model.ShelterInfo;
+
+public class ListActivity extends Activity {
+
+    private ListView lv;
+    private ListAdapter adapter;
+
+    public void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_list);
+
+        lv = findViewById(R.id.list_view);
+
+        //Reading the CSV
+        InputStream inputStream = getResources().openRawResource(R.raw.data);
+        CsvFileReader csvFile = new CsvFileReader(inputStream);
+        List<ShelterInfo> shelterlist = csvFile.read();
+
+        //adding shelter names to the array list
+        List<String> namelist = new ArrayList<>();
+        for (ShelterInfo shelter: shelterlist) {
+            namelist.add(shelter.getShelterName());
+
+        }
+
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        adapter = new ListAdapter(ListActivity.this, namelist);
+        if (namelist != null) {
+            lv.setAdapter(adapter);
+        }
+
+        //single click
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // selected item
+                String name = ((TextView) view).getText().toString();
+
+                // Launching new Activity on selecting single List Item
+                Intent i = new Intent(getApplicationContext(), SingleListItem.class);
+                // sending data to new activity
+                i.putExtra("Name", name);
+                startActivity(i);
+
+            }
+        });
+    }
+}

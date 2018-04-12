f13e0ffe8968a4833dfe4312f784bfa9be64eafe
bsheppard8 <bsheppard8@gatech.edu>
2018-04-12 14:09:25 -0400

    Added tests for User.java

--- app/src/androidTest/java/edu/team27/perfectcube/model/RobertTest.java
+++ app/src/androidTest/java/edu/team27/perfectcube/model/RobertTest.java
@@ -25,6 +25,12 @@ public class RobertTest {
     private UserDatabase mDb;
     private static ArrayList<User> userInfo = new ArrayList<>();
 
+    private static User user1;
+    private static User user2;
+    private static User user3;
+    private static User user4;
+    private static User user5;
+
     @Before
     public void createDb() {
         Context context = InstrumentationRegistry.getTargetContext();
@@ -36,9 +42,34 @@ public class RobertTest {
         LoginData.setUserInfo(userInfo);
     }
 
+    @Before
+    public void createUsers() {
+        user1 = new User("Jimmy", "password", UserType.USER, 1, "Here");
+        user2 = new User("Jimmy", "password", UserType.USER, 1, "Here");
+        user3 = new User("Taylor", "password", UserType.USER, 17, "Here");;
+        user4 = new User("Amara", "12345", UserType.USER, 23, "There");
+        user5 = new User("Christine", "openSesame", UserType.USER, 93, "Southwest");
+    }
+
     @Test
     public void getPass() throws Exception {
         assertEquals("whatever", LoginData.getPass("idunno"));
+    }
+
+    @Test
+    public void testUsers() {
+        assertTrue(user1.equals(user2));
+        assertFalse(user1.equals(user3));
+        assertFalse(user2.equals(user4));
+        assertFalse(user2.equals(user5));
+        assertTrue(!user1.getUsername().equals(user4.getUsername()));
+        assertFalse(user2.getUsername().equals(user3.getUsername()));
+        assertTrue(user1.getPassword().equals(user3.getPassword()));
+        assertFalse(user2.getPassword().equals(user4.getPassword()));
+        assertTrue(user3.getReservationNumber() != user5.getReservationNumber());
+        assertFalse(user4.getReservationNumber() == user5.getReservationNumber());
+        assertTrue(user1.getReservationLocation().equals(user3.getReservationLocation()));
+        assertFalse(user2.getReservationLocation().equals(user5.getReservationLocation()));
     }
 
     @After

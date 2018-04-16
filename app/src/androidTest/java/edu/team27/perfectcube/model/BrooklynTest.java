d7e41cf66418d319f269fe91a19edc2b9b0323ad
bsheppard8 <bsheppard8@gatech.edu>
2018-04-16 14:34:50 -0400

    Brooklyn's JUnit Test

--- app/src/androidTest/java/edu/team27/perfectcube/model/BrooklynTest.java
+++ app/src/androidTest/java/edu/team27/perfectcube/model/BrooklynTest.java
@@ -17,13 +17,10 @@ import java.util.Arrays;
 import static org.junit.Assert.*;
 
 /**
- * Created by Robert on 4/9/2018.
+ * Created by Brooklyn on 4/9/2018.
  */
 @RunWith(AndroidJUnit4.class)
-public class RobertTest {
-    private UserDao mUserDao;
-    private UserDatabase mDb;
-    private static ArrayList<User> userInfo = new ArrayList<>();
+public class BrooklynTest {
 
     private static User user1;
     private static User user2;
@@ -32,28 +29,12 @@ public class RobertTest {
     private static User user5;
 
     @Before
-    public void createDb() {
-        Context context = InstrumentationRegistry.getTargetContext();
-        mDb = Room.databaseBuilder(context,
-                UserDatabase.class, "user-database").allowMainThreadQueries()
-                .build();
-        mUserDao = mDb.getUserDao();
-        userInfo.addAll(Arrays.asList(mUserDao.loadAllUsers()));
-        LoginData.setUserInfo(userInfo);
-    }
-
-    @Before
     public void createUsers() {
         user1 = new User("Jimmy", "password", UserType.USER, 1, "Here");
         user2 = new User("Jimmy", "password", UserType.USER, 1, "Here");
         user3 = new User("Taylor", "password", UserType.USER, 17, "Here");;
         user4 = new User("Amara", "12345", UserType.USER, 23, "There");
         user5 = new User("Christine", "openSesame", UserType.USER, 93, "Southwest");
-    }
-
-    @Test
-    public void getPass() throws Exception {
-        assertEquals("whatever", LoginData.getPass("idunno"));
     }
 
     @Test
@@ -70,10 +51,5 @@ public class RobertTest {
         assertFalse(user4.getReservationNumber() == user5.getReservationNumber());
         assertTrue(user1.getReservationLocation().equals(user3.getReservationLocation()));
         assertFalse(user2.getReservationLocation().equals(user5.getReservationLocation()));
-    }
-
-    @After
-    public void closeDb() throws IOException {
-        mDb.close();
     }
 }

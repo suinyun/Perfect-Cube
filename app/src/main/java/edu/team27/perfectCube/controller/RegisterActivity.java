21b41a6b84e4e56574612d20a0c780cc99a8a03d
emmadoksansky <emma.doksansky@gmail.com>
2018-04-17 00:59:08 -0400

    checkstyled to the best of emma's ability. updates in groupMe. 4/17.

--- app/src/main/java/edu/team27/perfectCube/controller/RegisterActivity.java
+++ app/src/main/java/edu/team27/perfectCube/controller/RegisterActivity.java
@@ -1,33 +1,36 @@
-package edu.team27.perfectcube.controller;
+package edu.team27.perfectCube.controller;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckedTextView;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-import edu.team27.perfectcube.R;
-import edu.team27.perfectcube.model.LoginData;
-import edu.team27.perfectcube.model.User;
-import edu.team27.perfectcube.model.UserDatabase;
-import edu.team27.perfectcube.model.UserType;
+import edu.team27.perfectCube.R;
+import edu.team27.perfectCube.model.LoginData;
+import edu.team27.perfectCube.model.User;
+import edu.team27.perfectCube.model.UserDatabase;
+import edu.team27.perfectCube.model.UserType;
 
+/**
+ * allows a user to register
+ * creates that user
+ *
+ * */
 public class RegisterActivity extends AppCompatActivity {
 
-    private Activity a = this;
-    Button submitButton;
-    EditText username;
-    EditText password;
-    TextView warning;
-    Spinner userTypeSpinner;
+    private final Activity a = this;
+    private EditText username;
+    private EditText password;
+    private TextView warning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +39,16 @@ public class RegisterActivity extends AppCompatActivity {
 
         LoginData.updateUserInfo();
 
-        submitButton = findViewById(R.id.submitButton);
+        Button submitButton = findViewById(R.id.submitButton);
         username = findViewById(R.id.usernameText);
         password = findViewById(R.id.passwordText);
         warning = findViewById(R.id.warning);
-        userTypeSpinner = findViewById(R.id.spinner);
+        Spinner userTypeSpinner = findViewById(R.id.spinner);
         /*
           Set up the adapter to display the allowable majors in the spinner
          */
-        ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, UserType.values());
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, UserType.values());
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         userTypeSpinner.setAdapter(adapter);
 
@@ -57,22 +61,22 @@ public class RegisterActivity extends AppCompatActivity {
                 ArrayList<User> users = LoginData.getUserInfo();
                 //ArrayList<String> passwords = LoginData.getPasswords();
 
+                Editable u = username.getText();
+                Editable p = password.getText();
 
-
-                if (LoginData.findUser(username.getText().toString())) {
+                if (LoginData.findUser(u.toString())) {
                         //username is taken
                         warning.setVisibility(View.VISIBLE);
                 } else {
-                        //I changed the last parameter in the addUser call. We can expect all registering members to be USERS, right?
-                        LoginData.addUser(username.getText().toString(),password.getText().toString(),UserType.USER,0,"");
-                        //LoginData.db.userDao().insertUsers(new User(username.getText().toString(), password.getText().toString(),
-                                //UserType.USER,1,"")); //This line was added to add the user to the database
+                        //I changed the last parameter in the addUser call.
+                    // We can expect all registering members to be USERS, right?
+                        LoginData.addUser(u.toString(), p.toString(),
+                                UserType.USER,0,"");
                         Intent intent = new Intent(a, LoginActivity.class);
                         startActivity(intent);
                 }
 
-                    User newUser = new User(username.getText().toString(),
-                            password.getText().toString(),
+                    User newUser = new User(u.toString(), p.toString(),
                             UserType.USER, 0, "");
                     users.add(newUser);
                     //passwords.add(password.getText().toString());

7f58d4a757c99f9b3d45e84156bbde238fbf830d
bsheppard8 <bsheppard8@gatech.edu>
2018-04-27 09:49:12 -0400

    Finished GoogleMap Activity and added zoom button

--- app/src/main/java/edu/team27/perfectCube/controller/MapsActivity.java
+++ app/src/main/java/edu/team27/perfectCube/controller/MapsActivity.java
@@ -3,9 +3,11 @@ package edu.team27.perfectCube.controller;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -54,7 +56,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mo = mo.position(ll);
             mo = mo.title(shelter.getShelterName());
             googleMap.addMarker(mo).setSnippet("Phone Number: " + shelter.getPhoneNumber());
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(ll));
         }
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
         // Add a marker in Sydney and move the camera
         /*LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
